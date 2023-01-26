54dc449ab72bb2b6b8b2b89da42993e089bc6dfc
Alline Ribeiro <allinesineiro@gmail.com>
2023-01-26 17:32:48 +0000

    Configuração cypress grep

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,6 +3,8 @@ import 'cypress-iframe'
 import 'cypress-mailosaur'
 import './commands'
 
+require('cypress-grep')()
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
@@ -11,3 +13,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false
 })
+
+module.exports = (on, config) => {
+  require('cypress-grep/src/plugin')(config)
+}
